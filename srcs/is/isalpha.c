c632a62fe1fb7a4fafbf6ce2ccb4930bb694d795
Yawa <61963667+Yayawak@users.noreply.github.com>
2023-02-22 11:40:19 +0700

    finished isalpha

---
+++ srcs/is/isalpha.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int ft_isalpha(char);
+
+int main(int argc, char** argv)
+{
+    char    c;
+
+    if (argc == 2)
+    {
+        /* c = **argv; */
+        printf("*argv[0] = %c", *argv[0]); // name of program
+        printf("*argv[1] = %c", *argv[1]);
+        c = *argv[1];
+    }
+    printf("Number of argc = %d\n", argc);
+    printf("%d\n", ft_isalpha(c));
+
+    return (0);
+}
+
+int ft_isalpha(char c)
+{
+
+    /* printf("c = %d\n", c); */
+    /* if ((c >= 65 && c <= 90) || */
+    /*     (c >= 97 && c <= 122)) */
+    if ((c >= 'A' && c <= 'Z') ||
+        (c >= 'a' && c <= 'z'))
+    {
+        return (1);
+    }
+    return (0);
+}
+
